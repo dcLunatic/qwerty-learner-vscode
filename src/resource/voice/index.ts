5997f7dc3f6108ceddf480aa4a8a1e0b27bad14e
dcLunatic <dcLunatic6@gmail.com>
2024-03-08 11:28:43 +0800

    更换牛津词典发音mp3地址的获取方式

--- src/resource/voice/index.ts
+++ src/resource/voice/index.ts
@@ -90,9 +90,20 @@ function getMp3ByIciba(word: string, callback: PhoneCallback) {
 }
 
 function getMp3ByOxford(word: string, callback: PhoneCallback) {
-  const ukphone = `https://ssl.gstatic.com/dictionary/static/sounds/oxford/${word}--_gb_1.mp3`
-  const usphone = `https://ssl.gstatic.com/dictionary/static/sounds/oxford/${word}--_us_1.mp3`
-  callback(ukphone, usphone)
+  const url = `https://www.oxfordlearnersdictionaries.com/definition/english/${word}`
+  const headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
+  axios.get(url, { headers }).then((res) => {
+    const regex = /data-src-mp3="(.*?gb_1.mp3)"[\s\S]*?data-src-mp3="(.*?us_1.mp3)"/;
+    const match = res.data.match(regex);
+    if (match) {
+      callback(match[1], match[2])
+    } else {
+      callback(undefined, undefined)
+    }
+  }).catch((err) => {
+    console.error(err)
+    callback(undefined, undefined)
+  })
 }
 
 const voiceSourceFuncMap: Record<string, (word: string, callback: PhoneCallback) => void> = {
