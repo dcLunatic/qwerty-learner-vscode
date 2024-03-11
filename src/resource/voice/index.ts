1425bb560b5f4b03078d01f2fa894496c4c27818
dcLunatic <dcLunatic6@gmail.com>
2024-03-11 11:51:15 +0800

    增加剑桥词典发音源

--- src/resource/voice/index.ts
+++ src/resource/voice/index.ts
@@ -38,12 +38,12 @@ if (!(NATIVE && NATIVE.playerPlay)) {
   NATIVE = null
 }
 
-type PhoneCallback = (ukphone: string|undefined, usphone: string|undefined) => void
+type PhoneCallback = (ukphone: string|null, usphone: string|null) => void
 
 function getMp3ByDictCn(word: string, callback: PhoneCallback) {
   axios.get(`https://dict.cn/search?q=${word}`).then((res) => {
     if (res.status !== 200) {
-      callback(undefined, undefined)
+      callback(null, null)
       return
     }
     const regex = /<i class="sound fsound" naudio="([^"]+)"/g;  // 女声
@@ -53,16 +53,16 @@ function getMp3ByDictCn(word: string, callback: PhoneCallback) {
     let match;
   
     while ((match = regex.exec(res.data)) !== null) {
-        matches.push(match[1]);
+        matches.push('https://audio.dict.cn/' + match[1]);
     }
     if (matches.length >= 2) {
-        callback('https://audio.dict.cn/' + matches[0], 'https://audio.dict.cn/' + matches[1]) // 英式在第一二个位置，美式在第三四个位置
+        callback(matches[0], matches[1]) // 英式在第一二个位置，美式在第三四个位置
     } else {
-        callback(undefined, undefined)
+        callback(null, null)
     }
   }).catch((err) => {
     console.error(err)
-    callback(undefined, undefined)
+    callback(null, null)
   })
 }
 
@@ -81,11 +81,11 @@ function getMp3ByIciba(word: string, callback: PhoneCallback) {
     if (match) {
       callback(match[2], match[1])
     } else {
-        callback(undefined, undefined)
+        callback(null, null)
     }
   }).catch((err) => {
     console.error(err)
-    callback(undefined, undefined)
+    callback(null, null)
   })
 }
 
@@ -98,12 +98,42 @@ function getMp3ByOxford(word: string, callback: PhoneCallback) {
     if (match) {
       callback(match[1], match[2])
     } else {
-      callback(undefined, undefined)
+      callback(null, null)
     }
   }).catch((err) => {
     console.error(err)
-    callback(undefined, undefined)
+    callback(null, null)
   })
+}
+
+import https from 'https'; 
+function getMp3ByCambridge(word: string, callback: PhoneCallback) {
+  const url = `https://dictionary.cambridge.org/us/dictionary/english/${word}`
+  const headers = {'Host': 'dictionary.cambridge.org', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
+  const options = {
+    headers: headers
+  };
+  console.log(url)
+  https.get(url, options, (response) => {
+    let data = '';
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+    response.on('end', () => {
+      console.log("end", url)
+      const ukregex = /<span class="uk dpron-i ">[\s\S]*?<source type="audio\/mpeg" src="(.*?)"/
+      const usregex = /<span class="us dpron-i ">[\s\S]*?<source type="audio\/mpeg" src="(.*?)"/
+      let ukphone = null, usphone = null
+      let match;
+      match = data.match(ukregex);
+      if (match) {ukphone = 'https://dictionary.cambridge.org' + match[1];}
+      match = data.match(usregex);
+      if (match) {usphone = 'https://dictionary.cambridge.org' + match[1];}
+      callback(ukphone, usphone)
+    });
+  }).on('error', (error) => {
+    console.error(`Error: ${error.message}`);
+  });
 }
 
 const voiceSourceFuncMap: Record<string, (word: string, callback: PhoneCallback) => void> = {
@@ -111,16 +141,17 @@ const voiceSourceFuncMap: Record<string, (word: string, callback: PhoneCallback)
   "牛津词典": getMp3ByOxford,
   "有道词典": getMp3ByYoudao,
   "海词词典": getMp3ByDictCn,
+  "剑桥词典": getMp3ByCambridge,
 }
 
-
+const defaultVoiceMp3 = 'https://dict.youdao.com/dictvoice?audio=error&type=1'
 export const voicePlayer = (word: string, callback: () => void) => {
   const voiceSource = getConfig('voiceSource')
   const func = voiceSourceFuncMap[voiceSource]
   func(word, (ukphone, usphone)=>{
     if (NATIVE) {
       const url = getConfig('voiceType') === 'uk' ? ukphone : usphone;
-      NATIVE.playerPlay(url, callback)
+      NATIVE.playerPlay(url || defaultVoiceMp3, callback)
     } else {
       callback()
     }
