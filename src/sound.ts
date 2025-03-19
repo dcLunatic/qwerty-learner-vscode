080e9e10bd7c519a23c35bfeecd645e5faf7f8c4
dcLunatic <dcLunatic6@gmail.com>
2025-03-19 15:03:31 +0800

    采用更低延迟的wav播放方式

--- src/sound.ts
+++ src/sound.ts
@@ -1,12 +1,48 @@
-const wavPlayer = require('node-wav-player')
+// const wavPlayer = require('node-wav-player')
 import path from 'path'
 import { getConfig } from './utils'
+const fs = require('fs');
+const wav = require('wav');
+const Speaker = require('speaker');
 
 type SoundType = 'click' | 'wrong' | 'success'
+
+// export const soundPlayer = (type: SoundType) => {
+//   if (getConfig('keySound')) {
+//     let soundPath
+//     switch (type) {
+//       case 'click':
+//         soundPath = path.join(__dirname, '..', 'assets/sounds', 'click.wav')
+//         break
+//       case 'wrong':
+//         soundPath = path.join(__dirname, '..', 'assets/sounds', 'beep.wav')
+//         break
+//       case 'success':
+//         soundPath = path.join(__dirname, '..', 'assets/sounds', 'hint.wav')
+//         break
+//       default:
+//         break
+//     }
+//     // wavPlayer.stop()
+//     wavPlayer.play({ path: soundPath })
+//   }
+// }
+
+interface WavFormat {
+  audioFormat: number;
+  endianness: string;
+  channels: number;
+  sampleRate: number;
+  byteRate: number;
+  blockAlign: number;
+  bitDepth: number;
+  signed: boolean;
+}
 
 export const soundPlayer = (type: SoundType) => {
   if (getConfig('keySound')) {
     let soundPath
+    
     switch (type) {
       case 'click':
         soundPath = path.join(__dirname, '..', 'assets/sounds', 'click.wav')
@@ -20,7 +56,16 @@ export const soundPlayer = (type: SoundType) => {
       default:
         break
     }
-    // wavPlayer.stop()
-    wavPlayer.play({ path: soundPath })
+    const file = fs.createReadStream(soundPath);
+    const reader = new wav.Reader();
+    reader.on('format', (format: WavFormat) => {
+      console.log("Audio format:", format);
+      const speaker = new Speaker(format);
+      speaker.on('error', (err: any) => console.error("Speaker error:", err));
+      reader.pipe(speaker); 
+    });
+    file.on('error', (err: any) => console.error("File read error:", err));
+    reader.on('error', (err: any) => console.error("WAV decode error:", err));
+    file.pipe(reader);
   }
 }
